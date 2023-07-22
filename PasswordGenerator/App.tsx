047aa76358f456b14e18b6b9146c947fce91688e
Dhruv <dhruv903007@gmail.com>
2023-07-22 12:19:37 +0530

    Completed simple password generator

---
+++ PasswordGenerator/App.tsx
@@ -0,0 +1,218 @@
+import { Alert, Button, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import Clipboard from '@react-native-clipboard/clipboard';
+
+const App = () => {
+
+  const [passwordValue, setPasswordValue] = useState('Your password will be shown here')
+  const [lowercase, setLowercase] = useState(true)
+  const [uppercase, setUppercase] = useState(false)
+  const [number, setNumber] = useState(false)
+  const [specialCharacter, setSpecialCharacter] = useState(false)
+  const [inputValue, setInputValue] = useState('');
+
+  const generatePasswordString = () => {
+    let myPasswordString = ''
+    if (lowercase) {
+      myPasswordString += 'abcdefghijklmnopqrstuvwxyz';
+    }
+    if (uppercase) {
+      myPasswordString += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if (number) {
+      myPasswordString += '1234567890'
+    }
+    if (specialCharacter) {
+      myPasswordString += '@#$&?*%~/'
+    }
+    return myPasswordString;
+  }
+
+  const resetState = () => {
+    setUppercase(false),
+      setSpecialCharacter(false),
+      setNumber(false),
+      setInputValue('')
+    return
+  }
+
+  const copyPassword = (password:string) => {
+    Clipboard.setString(password);
+    Alert.alert("Password Copied!")
+  }
+
+
+  const generatePassword = async () => {
+    const passwordLength = parseInt(inputValue);
+    if (passwordLength > 18) {
+      Alert.alert("Password length cant be greater than 18!")
+      return
+    }
+    else if (passwordLength < 8) {
+      Alert.alert("Password should have at lest 8 character")
+      return
+    }
+
+    let myPassword = ''
+    const passwordString = generatePasswordString();
+    for (let i = 0; i < passwordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * passwordString.length)
+      myPassword += passwordString.charAt(randomIndex)
+    }
+    await setPasswordValue(myPassword)
+    resetState();
+  }
+
+  return (
+    <SafeAreaView>
+      <View style={styles.heading} >
+        <Text style={styles.headingText} >Password Generator</Text>
+        <Text style={styles.headingTextSecond}>You deserve a better password!</Text>
+      </View>
+
+      <View style={styles.generatedPasswordContainer} >
+        <Pressable>
+          <Text  onPress={()=>{copyPassword(passwordValue)}} style={styles.generatedPassword}>{passwordValue}</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.checkboxContainer} >
+
+        <View style={styles.checkbox} >
+          <BouncyCheckbox
+            disableBuiltInState
+            fillColor='black'
+            isChecked={number}
+            onPress={() => { setNumber(!number) }}
+          />
+          <Text style={styles.checkboxText}>Numeric character</Text>
+        </View>
+
+        <View style={styles.checkbox} >
+          <BouncyCheckbox
+            disableBuiltInState
+            fillColor='black'
+            isChecked={lowercase}
+            onPress={() => { setLowercase(!lowercase) }}
+          />
+          <Text style={styles.checkboxText}>Lowercase character</Text>
+        </View>
+
+        <View style={styles.checkbox} >
+          <BouncyCheckbox
+            disableBuiltInState
+            fillColor='black'
+            isChecked={uppercase}
+            onPress={() => { setUppercase(!uppercase) }}
+          />
+          <Text style={styles.checkboxText}>Uppercase character</Text>
+        </View>
+
+        <View style={styles.checkbox} >
+          <BouncyCheckbox
+            disableBuiltInState
+            fillColor='black'
+            isChecked={specialCharacter}
+            onPress={() => { setSpecialCharacter(!specialCharacter) }}
+          />
+          <Text style={styles.checkboxText}>Special character</Text>
+        </View>
+
+      </View>
+
+      <View style={styles.inputContainer} >
+        <TextInput keyboardType='numeric' style={styles.inputDec} value={inputValue} onChangeText={(text) => setInputValue(text)} placeholder='Enter the length of password' />
+      </View>
+
+      <View style={styles.buttonContainer} >
+        <Pressable>
+          <Text onPress={generatePassword} style={styles.button} >Generate</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  heading: {
+    backgroundColor: 'black',
+    height: 200,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  headingText: {
+    color: 'white',
+    fontSize: 35,
+    fontWeight: '700',
+  },
+  headingTextSecond: {
+    color: 'white'
+  },
+
+  generatedPasswordContainer: {
+    backgroundColor: 'white',
+    height: 150,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  generatedPassword: {
+    borderWidth: 2,
+    backgroundColor: 'white',
+    color: 'black',
+    width: 300,
+    textAlign: 'center',
+    height: 50,
+    textAlignVertical: 'center',
+    fontSize: 18
+  },
+  inputContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputDec: {
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 2,
+    width: 250,
+    paddingHorizontal: 15,
+    fontSize: 16
+  },
+  buttonContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    borderWidth: 2,
+    width: 250,
+    height: 50,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 25,
+    backgroundColor: 'black',
+    color: 'white',
+  },
+  checkboxContainer: {
+    height: 200,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+
+  checkbox: {
+    flexDirection: 'row',
+    marginBottom: 15,
+    paddingLeft: 90
+  },
+  checkboxText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+
+  }
+})
